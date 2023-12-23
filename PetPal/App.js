0cc2b6eea566e98a37eb1f28e15a86895170722c
melanie-attard <melattard15@gmail.com>
2023-12-23 12:14:20 +0100

    Bottom Navigation

--- PetPal/App.js
+++ PetPal/App.js
@@ -1,22 +1,80 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+import HomeScreen from "./screens/HomeScreen";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-      </View>
-    </SafeAreaProvider>
+    <>
+      <StatusBar style="auto" />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({ focused, color, size }) =>
+                focused == true ? (
+                  <MaterialCommunityIcons
+                    name="shopping"
+                    color={color}
+                    size={size}
+                  />
+                ) : (
+                  <MaterialCommunityIcons
+                    name="shopping-outline"
+                    color={color}
+                    size={size}
+                  />
+                ),
+            }}
+          />
+          <Tab.Screen
+            name="Cart"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({ focused, color, size }) =>
+                focused == true ? (
+                  <MaterialCommunityIcons
+                    name="cart"
+                    color={color}
+                    size={size}
+                  />
+                ) : (
+                  <MaterialCommunityIcons
+                    name="cart-variant"
+                    color={color}
+                    size={size}
+                  />
+                ),
+            }}
+          />
+          <Tab.Screen
+            name="Account"
+            component={HomeScreen}
+            options={{
+              tabBarIcon: ({ focused, color, size }) =>
+                focused == true ? (
+                  <MaterialCommunityIcons
+                    name="account"
+                    color={color}
+                    size={size}
+                  />
+                ) : (
+                  <MaterialCommunityIcons
+                    name="account-outline"
+                    color={color}
+                    size={size}
+                  />
+                ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
