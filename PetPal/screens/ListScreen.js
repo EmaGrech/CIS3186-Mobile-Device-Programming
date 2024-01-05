87c3ba1d2a3cc6611044d7218f62e7e9652963ef
EmaGrech <ema.grech.21@um.edu.mt>
2024-01-05 02:31:16 +0100

    Update ListScreen.js

--- PetPal/screens/ListScreen.js
+++ PetPal/screens/ListScreen.js
@@ -1,24 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, FlatList, Image, TouchableOpacity} from "react-native";
 import { useNavigation } from '@react-navigation/native';
-import { getDocsFromFirestore } from "../db";
+import { getCollFromFirestore } from "../db";
 
 //rn only fetches EVERY PRODUCT (need to filter according to product type)
 const ListScreen = () => {
   const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
   const navigation = useNavigation();
 
+  
   useEffect(() => {
-    const fetchProducts = async () => {
-      const data = await getDocsFromFirestore("Product Details");
-      setProducts(data);
+    const fetchData = async () => {
+      try
+      {
+        const data = await getCollFromFirestore("Product_Details");
+        setProducts(data);
+        setError(null);
+      }
+      catch (error)
+      {
+        console.error("Error fetching data:", error);
+        setError("Error fetching data");
+      }
     };
 
-    fetchProducts();
-  }, []);
+    const focusHandler = navigation.addListener("focus", fetchData);
+
+    return() => {
+      focusHandler();
+    } ;
+  }, [navigation]);
 
   const handleItemClick = (itemID, itemName) => {
-    navigation.navigate('InfoScreen', { itemID, itemName });
+    navigation.navigate('Info', { itemID, itemName });
   };
 
   return (
@@ -27,10 +42,10 @@ const ListScreen = () => {
         data={products}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => handleItemClick(item.id, item['Product Name'])}>
+          <TouchableOpacity onPress={() => handleItemClick(item.id)}>
             <View>
               <Image source={{ uri: item.Image }} style={{ width: 100, height: 100 }} />
-              <Text>{item["Product Name"]}</Text>
+              <Text>{item["Product_Name"]}</Text>
             </View>
           </TouchableOpacity>
         )}
