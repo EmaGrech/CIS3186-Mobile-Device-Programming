f950842b4d759cdd57c82fbaedbf16a674b271ef
EmaGrech <ema.grech.21@um.edu.mt>
2024-01-06 23:40:02 +0100

    Update HomeScreen.js

--- PetPal/screens/HomeScreen.js
+++ PetPal/screens/HomeScreen.js
@@ -4,11 +4,18 @@ function HomeScreen({ navigation }) {
   const goToListScreen = () => {
     navigation.navigate("List");
   };
+
+  const goToFormScreen = () => {
+    navigation.navigate("Form", {collName: "Product_Details"});
+  };
   
   return (
     <View>
       <TouchableOpacity onPress={goToListScreen}>
         <Text>List Screen</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={goToFormScreen}>
+        <Text>Form Screen</Text>
       </TouchableOpacity>
     </View>
   );
