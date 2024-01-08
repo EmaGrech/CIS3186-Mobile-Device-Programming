99b30fa319c74b228cd92c4b8e40ba46db49763e
EmaGrech <ema.grech.21@um.edu.mt>
2024-01-08 11:32:55 +0100

    Update ListScreen.js

--- PetPal/screens/ListScreen.js
+++ PetPal/screens/ListScreen.js
@@ -2,27 +2,20 @@ import React, { useEffect, useState } from "react";
 import { View, Text, FlatList, Image, TouchableOpacity} from "react-native";
 import { useNavigation } from '@react-navigation/native';
 import { getCollFromFirestore } from "../db";
+import RNPickerSelect from 'react-native-picker-select';
 
-//rn only fetches EVERY PRODUCT (need to filter according to product type)
 const ListScreen = () => {
   const [products, setProducts] = useState([]);
-  const [error, setError] = useState(null);
   const navigation = useNavigation();
 
+  const [filter, setFilter] = useState(null);
+  const [filterProducts, setFilterProducts] = useState([]);
   
   useEffect(() => {
     const fetchData = async () => {
-      try
-      {
-        const data = await getCollFromFirestore("Product_Details");
-        setProducts(data);
-        setError(null);
-      }
-      catch (error)
-      {
-        console.error("Error fetching data:", error);
-        setError("Error fetching data");
-      }
+      const data = await getCollFromFirestore("Product_Details");
+      setProducts(data);
+      setFilterProducts(data);
     };
 
     const focusHandler = navigation.addListener("focus", fetchData);
@@ -32,17 +25,39 @@ const ListScreen = () => {
     } ;
   }, [navigation]);
 
-  const handleItemClick = (itemID, itemName) => {
+  const handleSelect = (itemID, itemName) => {
     navigation.navigate('Info', { itemID, itemName });
+  };
+
+  const handleFilter = (value) => {
+    setFilter(value);
+    if (value === null) {
+      setFilterProducts(products);
+    } else {
+      const filtered = products.filter(item => item["Category"] === value);
+      setFilterProducts(filtered);
+    }
   };
 
   return (
     <View>
+      <RNPickerSelect
+        onValueChange={(value) => handleFilter(value)}
+        items={[
+          { label: 'Accessories', value: 'Accessories' },
+          { label: 'Supplies', value: 'Supplies' },
+          { label: 'Food', value: 'Food' },
+          { label: 'Pet Essentials', value: 'Pet Essentials' },
+        ]}
+        placeholder={{ label: 'All', value: null }}
+        value={filter}
+      />
+
       <FlatList
-        data={products}
+        data={filterProducts}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => handleItemClick(item.id)}>
+          <TouchableOpacity onPress={() => handleSelect(item.id)}>
             <View>
               <Image source={{ uri: item.Image }} style={{ width: 100, height: 100 }} />
               <Text>{item["Product_Name"]}</Text>
