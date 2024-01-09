eb5f3c36af272969693f9e26aff62abaf5a5cc0b
EmaGrech <ema.grech.21@um.edu.mt>
2024-01-09 11:03:23 +0100

    Create style.js

---
+++ PetPal/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  input: {
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 5,
+    height: 40,
+    marginBottom: 5,
+    borderRadius: 10,
+  },
+  button: {
+    backgroundColor: '#70afe6',
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 25,
+  },
+});
+
+export default styles;
