15e47ff26490377fedb2ed29bb89a546c3780c94
EmaGrech <ema.grech.21@um.edu.mt>
2024-01-09 12:07:34 +0100

    Update style.js

--- PetPal/style.js
+++ PetPal/style.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
+    listContainer: {
         padding: 10,
     },
     listItemContainer: {
@@ -9,23 +9,23 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 10,
     },
-    image: {
+    listImage: {
         width: 100,
         height: 100,
         marginRight: 10,
     },
-    textContainer: {
+    listTextContainer: {
         flex: 1,
     },
-    productName: {
+    listProductName: {
         fontSize: 16,
         marginBottom: 5,
     },
-    price: {
+    listPrice: {
         marginTop: 5,
         fontWeight: 'bold',
     },
-    input: {
+    formInput: {
         borderColor: 'gray',
         borderWidth: 1,
         padding: 5,
@@ -33,11 +33,35 @@ const styles = StyleSheet.create({
         marginBottom: 5,
         borderRadius: 10,
     },
-    button: {
+    formButton: {
         backgroundColor: '#70afe6',
         padding: 10,
         alignItems: 'center',
         borderRadius: 25,
+    },
+    infoContainer: {
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    padding: 20,
+    },
+    infoImage: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+    },
+    infoProductName: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    },
+    infoDescription: {
+    marginBottom: 10,
+    },
+    infoPrice: {
+    marginBottom: 10,
+    },
+    infoStock: {
+    marginBottom: 20,
     },
 });
 
