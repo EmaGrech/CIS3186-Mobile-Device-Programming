d42c33910822879dcccccd615d17c12a6ee58368
EmaGrech <ema.grech.21@um.edu.mt>
2024-01-15 10:58:17 +0100

    Update FirebaseConfig.js

--- PetPal/FirebaseConfig.js
+++ PetPal/FirebaseConfig.js
@@ -12,6 +12,39 @@ const firebaseConfig = {
   appId: "1:41137584387:web:03d810aa41373ce3aad136",
 };
 
+/* 2nd version
+apiKey: "AIzaSyB7B3H_e8SZOmfdeNrxC6bX9sgETO4NVTQ",
+  authDomain: "pet-app-78d75.firebaseapp.com",
+  projectId: "pet-app-78d75",
+  storageBucket: "pet-app-78d75.appspot.com",
+  messagingSenderId: "956055289557",
+  appId: "1:956055289557:web:1cb777f5f856d5267e2c5c",
+
+*/
+/* apiKey: "AIzaSyCx4wJyVTxKfq4DAnyli_d6JzRuROUGEFs",
+  authDomain: "mobile-programming-589ec.firebaseapp.com",
+  projectId: "mobile-programming-589ec",
+  storageBucket: "mobile-programming-589ec.appspot.com",
+  messagingSenderId: "928758235272",
+  appId: "1:928758235272:web:2a5adec721cfbac9161f3d",
+  measurementId: "G-4YQ4BHMGJH" */
+
+/* 
+ apiKey: "AIzaSyB7B3H_e8SZOmfdeNrxC6bX9sgETO4NVTQ",
+  authDomain: "pet-app-78d75.firebaseapp.com",
+  projectId: "pet-app-78d75",
+  storageBucket: "pet-app-78d75.appspot.com",
+  messagingSenderId: "956055289557",
+  appId: "1:956055289557:web:43553ff69fe64abc7e2c5c", */
+
+/*  apiKey: "AIzaSyCsgAil9Eviz-Ra4yujHnk3adAIoBpNHtA",
+  authDomain: "petpal-3f19d.firebaseapp.com",
+  projectId: "petpal-3f19d",
+  storageBucket: "petpal-3f19d.appspot.com",
+  messagingSenderId: "41137584387",
+  appId: "1:41137584387:web:03d810aa41373ce3aad136", */
+
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
