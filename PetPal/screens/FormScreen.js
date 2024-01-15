6387c54dea2e7ce0cfc9e9ad8d7ec7ec43a44e03
EmaGrech <ema.grech.21@um.edu.mt>
2024-01-15 06:56:10 +0100

    Update FormScreen.js

--- PetPal/screens/FormScreen.js
+++ PetPal/screens/FormScreen.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { setFieldType, toAddtoCollection,toUpdateDocument } from '../db';
 import style from '../style';
-import { View, Text, TouchableOpacity, TextInput, Alert, Image} from "react-native";
+import { View, Text, TouchableOpacity, TextInput, Alert, Image, ScrollView} from "react-native";
 import * as ImagePicker from 'expo-image-picker';
 import DropDown from "../components/Dropdown";
 import { ProductCategories, ServiceCategories, UserCategories, Activities} from "../Categories";
 import { useNavigation } from '@react-navigation/native';
-import { createNewUser, passwordValidation } from './LoginScreen';
-import { ref, uploadString, getDownloadURL } from 'firebase/storage';
+import { createNewUser } from './LoginScreen';
+import { ref, uploadString, getDownloadURL } from '../db';
 
 
 //generates a form corresponding to a particular collection
@@ -38,61 +38,58 @@ const FormScreen = ({ route }) => {
     });
   };
 
-  //makes sure that data is correct
-  const validation = async () => {
+
+  const submit = async () => {
     const empty = Object.entries(fieldTypes)
       .filter(([fieldName, fieldType]) => fieldType === 'string' && formData[fieldName] === '')
       .map(([fieldName]) => fieldName);
+  
+      const imageField = Object.entries(fieldTypes).find(([fieldName, fieldType]) => fieldType === 'image');
+      if (imageField) {
+        const [fieldName] = imageField;
+    
+        if (formData[fieldName] && formData[fieldName].uri) {
+          const response = await fetch(imageUri);
+          const blob = await response.blob();
+          const fileName = `${fieldName}_${Date.now()}`;
+          const storageRef = ref(storage, `images/${fileName}`);
 
-    //redirects to password validation
-    if(fromLogin)
-    {
-      //specifies password requirements
-      const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
-      //returns true or false according to if test is passed
-      const pass = regex.test(formData.password);
-      if(!pass)
-      {
-        Alert.alert(`Invalid password`);
-        return false;
-      }
-    }
+          console.log('Uploading image to Firebase Storage...');
+          await uploadString(storageRef, blob, 'data_url');
 
-    //checks no field is left empty
+          console.log('Image uploaded successfully.');
+
+          const downloadURL = await getDownloadURL(storageRef);
+          currentInputs(fieldName, downloadURL);
+            }
+          }
+
+    // Checks no field is left empty
     if (empty.length > 0) {
-      Alert.alert(`Please fill in all fields: ${empty.join(', ')}`);
-      return false;
-    }
-      
-    return true;
-  };
+      Alert.alert(`Please fill in all fields`);
+      return;
+    }    
 
-  const submit = async () => {
-    if (validation()) {
-      if (fromLogin && !editMode) {
-        const { user, uid } = await createNewUser(formData.Email, formData.Password);
-        const image = formData['Users.Profile_Picture'];
-        
-        const storageRef = ref(storage, `images/${Users.id}/${image.name}`);
-        await uploadString(storageRef, image.uri, 'data_url');
-        const downloadURL = await getDownloadURL(storageRef);
-
-        formData['Users.Profile_Picture'] = downloadURL;
-
-        toAddtoCollection(collName, formData, uid);        
-      }
-      else if (editMode)
-      {
-        await toUpdateDocument(collName, initialData.id, formData);
-      }
-      else if (!editMode && !fromLogin)
-      {
+    if (editMode) {
+      await toUpdateDocument(collName, initialData.id, formData);
+    } else {
+      // Check if it's a new entry (not in editMode) and not from login screen
+      if (!fromLogin) {
         await toAddtoCollection(collName, formData);
       }
-      navigation.goBack();
+      else {
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+        const pass = regex.test(formData.Password);
+        if (!pass) {
+          Alert.alert(`Password is not valid:`, `Password must have \n 1 Uppercase Letter \n 1 Lowercase Letter \n 1 Number \n At least 8 characters`);
+          return;
+        }
+        const { user, uid } = await createNewUser(formData.Email, formData.Password);
+        await toAddtoCollection(collName, formData, uid);
+      }
     }
+    navigation.goBack();
   };
-
 
   const selectImage = async (fieldName) => {
     try {
@@ -117,74 +114,77 @@ const FormScreen = ({ route }) => {
   };
   
   return (
-    <View style={style.formContainer}>
-      <Text style={style.formTitle}>{collName}</Text>
-      {Object.entries(fieldTypes).map(([fieldName, fieldType]) => (
-        <View key={fieldName} style={{ marginBottom: 10 }}>
-          {fieldType === 'string' ? (
-            <View style={{alignItems: 'center'}}>
-              <TextInput
-              style={style.formInput}
-              value={formData[fieldName]}
-              onChangeText={(text) => currentInputs(fieldName, text)}
-              placeholder={`${fieldName}`}
-              placeholderTextColor="gray"
-              multiline={true}
-              mode="outlined"
-            />
-            </View>
-          ) : fieldType === 'float' ? (
-            <View style={{alignItems: 'center'}}>
-              <TextInput
-                style={[style.formInput]}
-                keyboardType = 'numeric' 
+    <ScrollView contentContainerStyle={style.scrollContainer}>
+      <View style={style.formContainer}>
+        <Text style={style.formTitle}>{collName}</Text>
+        {Object.entries(fieldTypes).map(([fieldName, fieldType]) => (
+          <View key={fieldName} style={{ marginBottom: 10 }}>
+            {fieldType === 'string' ? (
+              <View style={{alignItems: 'center'}}>
+                <TextInput
+                style={style.formInput}
                 value={formData[fieldName]}
                 onChangeText={(text) => currentInputs(fieldName, text)}
                 placeholder={`${fieldName}`}
                 placeholderTextColor="gray"
+                multiline={true}
                 mode="outlined"
               />
-            </View>
-          ) : fieldType === 'int' ? (
-            <View style={{alignItems: 'center'}}>
-            <TextInput
-              style={[style.formInput]}
-              value={formData[fieldName]}
-              onChangeText={(text) => currentInputs(fieldName, text)}
-              placeholder={`${fieldName}`}
-              placeholderTextColor="gray"
-              keyboardType="numeric"
-              mode="outlined"
-            />
-            </View>
-          ) : fieldType === 'image' ? (
-            <View style={{alignItems: 'center'}}>
-              <TouchableOpacity onPress={() => selectImage(fieldName)} >
-                <Text>Upload Image</Text>
-              </TouchableOpacity>
-              {formData[fieldName] && formData[fieldName].uri && (
-                <Image
-                  source={{ uri: formData[fieldName].uri }}
-                  style={{ width: 200, height: 200 }}
+              </View>
+            ) : fieldType === 'float' ? (
+              <View style={{alignItems: 'center'}}>
+                <TextInput
+                  style={[style.formInput]}
+                  keyboardType = 'numeric' 
+                  value={formData[fieldName]}
+                  onChangeText={(text) => currentInputs(fieldName, text)}
+                  placeholder={`${fieldName}`}
+                  placeholderTextColor="gray"
+                  mode="outlined"
                 />
-              )}
-            </View>
-          ) : fieldType === ('drop' || 'drop[]') ? (
-                fieldName === 'Activities' ? (
-                  <DropDown data={Activities} onValueChange={(value) => currentInputs(fieldName, value)}/>
-                ) : fieldName === 'Category' ? (
-                  <DropDown data={ProductCategories} onValueChange={(value) => currentInputs(fieldName, value)}/>
-                ) : fieldName === 'Account_Type' ? (
-                  <DropDown data={UserCategories} onValueChange={(value) => currentInputs(fieldName, value)}/>
+              </View>
+            ) : fieldType === 'int' ? (
+              <View style={{alignItems: 'center'}}>
+              <TextInput
+                style={[style.formInput]}
+                value={formData[fieldName]}
+                onChangeText={(text) => currentInputs(fieldName, text)}
+                placeholder={`${fieldName}`}
+                placeholderTextColor="gray"
+                keyboardType="numeric"
+                mode="outlined"
+              />
+              </View>
+            ) : fieldType === 'image' ? (
+              <View style={{alignItems: 'center'}}>
+                <TouchableOpacity onPress={() => selectImage(fieldName)} >
+                  <Text>Upload Image</Text>
+                </TouchableOpacity>
+                {formData[fieldName] && formData[fieldName].uri && (
+                  <Image
+                    source={{ uri: formData[fieldName].uri }}
+                    style={{ width: 200, height: 200 }}
+                  />
+                )}
+              </View>
+            ) : fieldType === ('drop' || 'drop[]') ? (
+                  fieldName === 'Activities' ? (
+                    <DropDown data={Activities} onValueChange={(value) => currentInputs(fieldName, value)}/>
+                  ) : fieldName === 'Category' ? (
+                    <DropDown data={ProductCategories} onValueChange={(value) => currentInputs(fieldName, value)}/>
+                  ) : fieldName === 'Account_Type' ? (
+                    <DropDown data={UserCategories} onValueChange={(value) => currentInputs(fieldName, value)}/>
+                  ) : null
                 ) : null
-              ) : null
-            }
-        </View>
-      ))}
-      <TouchableOpacity onPress={submit} style={style.formButton}>
-        <Text style={{ color: 'white' }}>Save</Text>
-      </TouchableOpacity>
-    </View>
+              }
+          </View>
+        ))}
+
+        <TouchableOpacity onPress={submit} style={style.formButton}>
+          <Text style={{ color: 'white' }}>Save</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
   );
 };
 
