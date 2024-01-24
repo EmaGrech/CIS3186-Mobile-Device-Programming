e865a6adeb7cb4f23527709272489099146d8749
EmaGrech <ema.grech.21@um.edu.mt>
2024-01-24 20:39:47 +0100

    Update Welcome.js

--- PetPal/screens/Welcome.js
+++ PetPal/screens/Welcome.js
@@ -40,7 +40,7 @@ const Welcome = ({ navigation }) => {
                     <View style={{marginTop:250, width:"80%", justifyContent:"center"}}>
                     <Button
                         title="Sign Up"
-                        onPress={() => navigation.navigate("Signup")}
+                        onPress={() => navigation.navigate("Form", {collName: 'Users', editMode: false, fromLogin: true})}
                     />
 
                     <View style={{marginTop:20}}>
